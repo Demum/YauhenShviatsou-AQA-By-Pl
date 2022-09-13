96c23ceb18d39eb7752d7ec58bfbc2ebf559990f
Yauhen Shviatsou <demum1930@gmail.com>
2022-09-13 12:13:32 +0300

    refactor NumberMoreThenSeven

--- src/main/java/NumberMoreThenSeven.java
+++ src/main/java/NumberMoreThenSeven.java
@@ -1,12 +1,20 @@
+import java.sql.SQLOutput;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class NumberMoreThenSeven {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter number");
-        int number = input.nextInt();
-        if(number>7){
-            System.out.println("Привет");
-        }
+       try {
+           int number = input.nextInt();
+           if(number>7){
+               System.out.println("Привет");
+           }
+       } catch (InputMismatchException e) {
+           System.out.println("Incorrect number");
+           e.printStackTrace();
+       }
     }
 }
