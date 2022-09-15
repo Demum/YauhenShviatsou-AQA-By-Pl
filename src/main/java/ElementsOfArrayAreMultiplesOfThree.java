8655a3b97c758a5aa2bf8158dd9327ceb3e4d33d
Demum <69975105+Demum@users.noreply.github.com>
2022-09-15 11:15:06 +0300

    Feature (#2)
    * add class NumberMoreThenSeven
    
    * add class Vyacheslav and gitIgnore
    
    * refactor NumberMoreThenSeven
    
    * add ElementsOfArrayAreMultiplesOfThree
    
    * add ElementsOfArrayAreMultiplesOfThree try catch
    
    * add class Type and code refactor
    
    * code refactor

--- src/main/java/ElementsOfArrayAreMultiplesOfThree.java
+++ src/main/java/ElementsOfArrayAreMultiplesOfThree.java
@@ -1,11 +1,9 @@
-import java.util.InputMismatchException;
-
 public class ElementsOfArrayAreMultiplesOfThree {
     public static void main(String[] args) {
         System.out.println("Enter array length: ");
         try {
             int sizeOfArray = Type.typeInt();
-            Double array[] = new Double[sizeOfArray];
+            Double[] array = new Double[sizeOfArray];
             System.out.println("Insert array elements:");
             for (int i = 0; i < sizeOfArray; i++) {
                 array[i] = Type.typeDouble();
@@ -15,9 +13,10 @@ public class ElementsOfArrayAreMultiplesOfThree {
                 if (array[i] % 3 == 0)
                     System.out.print(" " + array[i]);
             }
-               } catch (InputMismatchException e) {
-            System.out.println("Incorrect number: " + Type.typeString());
+        } catch (NumberFormatException e) {
+            System.out.println(e.toString());
             e.printStackTrace();
         }
+        Type.closeScanner();
     }
 }
--- src/main/java/NumberMoreThenSeven.java
+++ src/main/java/NumberMoreThenSeven.java
@@ -1,19 +1,16 @@
-import java.util.InputMismatchException;
-
 public class NumberMoreThenSeven {
-
     public static void main(String[] args) {
-        Double number=0.0;
-         System.out.println("Enter number");
-       try {
-           number = Type.typeDouble();
-       } catch (InputMismatchException e) {
-           System.out.println("Incorrect number: "+ Type.typeString());
-           e.printStackTrace();
-       }
-           if(number>7){
-               System.out.println("Привет");
-           }
-
+        Double number = 0.0;
+        System.out.println("Enter number");
+        try {
+            number = Type.typeDouble();
+        } catch (NumberFormatException e) {
+            System.out.println(e.toString());
+            e.printStackTrace();
+        }
+        if (number > 7) {
+            System.out.println("Привет");
+        }
+        Type.closeScanner();
     }
 }
